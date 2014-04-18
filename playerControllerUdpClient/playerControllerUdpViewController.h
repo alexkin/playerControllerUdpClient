10ff26323c0ff9cdf3d40009b9b0497c32b4113a
alexking <jasonmrliu@gmail.com>
2014-04-18 09:49:08 +0800

    Initial Commit

---
+++ playerControllerUdpClient/playerControllerUdpViewController.h
@@ -0,0 +1,13 @@
+//
+//  playerControllerUdpViewController.h
+//  playerControllerUdpClient
+//
+//  Created by stone on 14-4-18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface playerControllerUdpViewController : UIViewController
+
+@end
---
+++ playerControllerUdpClient/playerControllerUdpViewController.m
@@ -0,0 +1,29 @@
+//
+//  playerControllerUdpViewController.m
+//  playerControllerUdpClient
+//
+//  Created by stone on 14-4-18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "playerControllerUdpViewController.h"
+
+@interface playerControllerUdpViewController ()
+
+@end
+
+@implementation playerControllerUdpViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
