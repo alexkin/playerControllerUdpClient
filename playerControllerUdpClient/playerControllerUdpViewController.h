f6db153f8fa693ac00ad74e2e6e51e3b2d6a3770
alexking <jasonmrliu@gmail.com>
2014-04-18 16:40:24 +0800

    playerControllUdp_iPhone_iPad

--- playerControllerUdpClient/playerControllerUdpViewController.h
+++ playerControllerUdpClient/playerControllerUdpViewController.h
@@ -7,7 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "passValueByDelegate.h"
 
-@interface playerControllerUdpViewController : UIViewController
+@interface playerControllerUdpViewController : UIViewController<passValueByDelegate>
+
+@property(nonatomic,strong)NSString *myTextIp;
+@property(nonatomic,strong)NSString *myTextPort;
+
+//@property(retain,nonatomic)IBOutlet UITextField *textFieldIpInput;
+//@property(retain,nonatomic)IBOutlet UITextField *textFieldPortInput;
+
+@property(nonatomic,retain)IBOutlet UIButton *playPause;
+@property(nonatomic,retain)IBOutlet UIButton *mute;
+@property(nonatomic,retain)IBOutlet UIButton *playStop;
+@property(nonatomic,retain)IBOutlet UIButton *voiceAdd;
+@property(nonatomic,retain)IBOutlet UIButton *voiceMult;
+
+-(IBAction)button0:(id)sender;
+-(IBAction)button1:(id)sender;
+-(IBAction)button2:(id)sender;
+-(IBAction)button3:(id)sender;
+-(IBAction)button4:(id)sender;
+-(IBAction)button5:(id)sender;
+
+-(IBAction)playPause:(id)sender;
+-(IBAction)mute:(id)sender;
+-(IBAction)playStop:(id)sender;
+-(IBAction)voiceAdd:(id)sender;
+-(IBAction)voiceMult:(id)sender;
+
+-(IBAction)showSetIpAndPort:(id)sender;
+
 
 @end
--- playerControllerUdpClient/playerControllerUdpViewController.m
+++ playerControllerUdpClient/playerControllerUdpViewController.m
@@ -7,17 +7,550 @@
 //
 
 #import "playerControllerUdpViewController.h"
+#import "setIpAndPortPage.h"
+#import "dateDelegate.h"
+#import "GCDAsyncUdpSocket.h"
+#import "DDLog.h"
 
-@interface playerControllerUdpViewController ()
+//static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+#define FORMAT(format,...) [NSString stringWithFormat:(format),##__VA_ARGS__]
+
+@interface playerControllerUdpViewController (){
+    long tag;
+    GCDAsyncUdpSocket *udpSocket;
+    
+    NSMutableString *log;
+}
 
 @end
 
-@implementation playerControllerUdpViewController
+@implementation playerControllerUdpViewController{
+    bool boolBtnPlayPause,boolBtnMute,boolBtnPlayStop,boolBtnVoiceAdd,boolBtnVoiceMult;
+    int intStateMent;
+    
+}
+
+@synthesize myTextIp;
+@synthesize myTextPort;
+
+@synthesize playPause;
+@synthesize mute;
+@synthesize playStop;
+@synthesize voiceAdd;
+@synthesize voiceMult;
+
+-(void)passValue:(dateDelegate *)value{
+    self.myTextIp = value.textIp;
+    self.myTextPort = value.textPort;
+    
+    NSLog(@"myTextIp here: %@",value.textIp);
+    
+    if (udpSocket == nil) {
+        [self setupSocket];
+    }
+}
+
+//Socket设置
+-(void)setupSocket{
+    udpSocket = [[GCDAsyncUdpSocket alloc]initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    
+    NSError *error = nil;
+    
+    if (![udpSocket bindToPort:0 error:&error]) {
+        [self logEror:FORMAT(@"Error binding:%@",error)];
+        return;
+    }
+    if (![udpSocket beginReceiving:&error]) {
+        [self logEror:FORMAT(@"Error receiving:%@",error)];
+        return;
+    }
+    [self logInfo:@"Ready"];
+}
+
+
+-(IBAction)showSetIpAndPort:(id)sender{
+    //setIpAndPortPage *controller =[[setIpAndPortPage alloc]initWithNibName:@"setIpAndPortPage" bundle:nil];
+    
+    //setIpAndPortPage *controller = [[setIpAndPortPage alloc]init];
+    
+    
+    UIStoryboard *storyBoard = [UIStoryboard storyboardWithName:@"Main_iPhone" bundle:nil];
+    setIpAndPortPage *controller = (setIpAndPortPage *)[storyBoard instantiateViewControllerWithIdentifier:@"ipSetting"];
+
+    //----------------------------------
+    controller.delegate = self;
+    
+    //controller.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    
+    [self performSegueWithIdentifier:@"ipSetting" sender:nil];
+    
+    
+    //[self presentViewController:controller animated:YES completion:nil];
+    
+    //controller.delegate = self;
+    //[self.navigationController pushViewController:controller animated:YES];
+    //controller.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+    //controller.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    //controller.modalTransitionStyle = UIModalTransitionStylePartialCurl;
+    //controller.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    //controller.modalTransitionStyle = UIModalPresentationPageSheet;
+    //[self presentViewController:controller animated:YES completion:nil];
+}
+
+-(void)setInitValue{
+    boolBtnPlayPause = false;
+    boolBtnMute = false;
+    boolBtnPlayStop = false;
+    boolBtnVoiceAdd = false;
+    boolBtnVoiceMult = false;
+    
+    intStateMent = -1;
+}
+
+//select content
+-(IBAction)button0:(id)sender{
+    intStateMent = 0;
+    
+    //------------------------
+    boolBtnPlayPause = false;
+    [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+    //------------------------
+    
+    
+    NSLog(@"here is the report:%@",myTextIp);
+    //NSLog(@"ahahhahaha %@",self.myTextPort);
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+    //send intStatement
+}
+
+-(IBAction)button1:(id)sender{
+    intStateMent = 1;
+    
+    //------------------------
+    boolBtnPlayPause = false;
+    [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+    //------------------------
+    
+    
+    
+    //**************************
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)button2:(id)sender{
+    intStateMent = 2;
+    
+    //------------------------
+    boolBtnPlayPause = false;
+    [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+    //------------------------
+    
+    
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)button3:(id)sender{
+    intStateMent = 3;
+    
+    //------------------------
+    boolBtnPlayPause = false;
+    [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+    //------------------------
+    
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)button4:(id)sender{
+    intStateMent = 4;
+    
+    //------------------------
+    boolBtnPlayPause = false;
+    [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+    //------------------------
+    
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)button5:(id)sender{
+    intStateMent = 5;
+    
+    //------------------------
+    boolBtnPlayPause = false;
+    [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+    //------------------------
+    
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+//set property of video
+-(IBAction)playPause:(id)sender{
+    //send
+    intStateMent = 6;
+    //[playPause setBackgroundImage:[UIImage imageNamed:@"add.png"] forState:UIControlStateNormal];
+    
+    //[playPause setBackgroundImage:[UIImage imageNamed:@"play-blue.png"] forState:UIControlStateHighlighted];
+    
+    
+    boolBtnPlayPause = !boolBtnPlayPause;
+    if (boolBtnPlayPause) {
+        NSLog(@"zhe shi yi ge ce shi : %d",boolBtnPlayPause);
+        [playPause setBackgroundImage:[UIImage imageNamed:@"noTouchPlay.png"] forState:UIControlStateNormal];
+        //[playPause setBackgroundImage:nil forState:UIControlStateNormal];
+    }
+    else{
+        [playPause setBackgroundImage:[UIImage imageNamed:@"yesTouchPlay.png"] forState:UIControlStateNormal];
+        NSLog(@"zhe shi yi ge ce shi : %d",boolBtnPlayPause);
+    }
+    
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+    
+    
+    //UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"1" message:@"2" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    //[alertView show];
+    
+    
+}
+
+-(IBAction)mute:(id)sender{
+    intStateMent = 7;
+    
+    //----------
+    boolBtnMute = !boolBtnMute;
+    if (boolBtnMute) {
+        [mute setBackgroundImage:[UIImage imageNamed:@"yesTouchMute.png"] forState:UIControlStateNormal];
+        //[playPause setBackgroundImage:nil forState:UIControlStateNormal];
+    }
+    else{
+        [mute setBackgroundImage:[UIImage imageNamed:@"noTouchMute.png"] forState:UIControlStateNormal];
+    }
+    //----------
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)playStop:(id)sender{
+    intStateMent = 8;
+    
+    //[playStop setBackgroundImage:[UIImage imageNamed:@"noTouchStop.png"] forState:UIControlStateNormal];
+    
+    [playStop setBackgroundImage:[UIImage imageNamed:@"yesTouchStop.png"] forState:UIControlStateHighlighted];
+    
+    [playPause setBackgroundImage:[UIImage imageNamed:@"noTouchPlay.png"] forState:UIControlStateNormal];
+    
+    boolBtnPlayPause = !boolBtnPlayPause;
+    //----------
+    boolBtnPlayStop = !boolBtnPlayStop;
+    /* if (boolBtnPlayStop) {
+     [playStop setBackgroundImage:[UIImage imageNamed:@"yesTouchStop.png"] forState:UIControlStateNormal];
+     //[playPause setBackgroundImage:nil forState:UIControlStateNormal];
+     }
+     else{
+     [playStop setBackgroundImage:[UIImage imageNamed:@"noTouchStop.png"] forState:UIControlStateNormal];
+     }
+     */
+    //----------
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)voiceAdd:(id)sender{
+    intStateMent = 9;
+    
+    //[voiceAdd setBackgroundImage:[UIImage imageNamed:@"noTouchAdd.png"] forState:UIControlStateNormal];
+    [voiceAdd setBackgroundImage:[UIImage imageNamed:@"yesTouchAdd.png"] forState:UIControlStateHighlighted];
+    //[playPause setBackgroundImage:nil forState:UIControlStateNormal];
+    //----------
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(IBAction)voiceMult:(id)sender{
+    intStateMent = 10;
+    
+    //[voiceMult setBackgroundImage:[UIImage imageNamed:@"noTouchMult.png"] forState:UIControlStateNormal];
+    [voiceMult setBackgroundImage:[UIImage imageNamed:@"yesTouchMult.png"] forState:UIControlStateHighlighted];
+    
+    
+    NSString *host = self.myTextIp;
+    if ([host length] == 0) {
+        [self logEror:@"Address required"];
+        return;
+    }
+    
+    int port = [self.myTextPort intValue];
+    if (port <= 0 || port > 65535) {
+        [self logEror:@"Valid port required"];
+        return;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"%d",intStateMent];
+    if ([msg length] == 0) {
+        [self logEror:@"Message required"];
+        return;
+    }
+    
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    
+    [self logMessage:FORMAT(@"SNT (%i): %@",(int)tag,msg)];
+    tag ++;
+}
+
+-(void)logEror:(NSString *)msg{
+    NSString *prefix = @"<font color =\"#B40404\">";
+    NSString *suffix = @"</font><br/>";
+    
+    [log appendFormat:@"%@%@%@\n",prefix,msg,suffix];
+}
+
+-(void)logMessage:(NSString *)msg{
+    NSString *prefix = @"<font color=\"#000000\">";
+    NSString *suffix = @"</font><br/>";
+    
+    [log appendFormat:@"%@%@%@\n",prefix,msg,suffix];
+}
+
+- (void)logInfo:(NSString *)msg
+{
+	NSString *prefix = @"<font color=\"#6A0888\">";
+	NSString *suffix = @"</font><br/>";
+	
+	[log appendFormat:@"%@%@%@\n", prefix, msg, suffix];
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setInitValue];
 }
 
 - (void)didReceiveMemoryWarning
